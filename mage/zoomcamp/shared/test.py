20cf393aa8ee4b0ccb74ac21fd3956384e0252e0
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2024-08-13 16:14:40 +0000

    added doc_id

--- mage/zoomcamp/shared/test.py
+++ mage/zoomcamp/shared/test.py
@@ -1,14 +1,15 @@
 from load_google_doc import read_faq
+from pprint import pprint
 import pandas as pd
 
 
 result = read_faq("1qZjwHkvP0lXHiE4zdbWyUXSVfmVGzougDD6N37bat3E")
-df = pd.DataFrame(result)
+# df = pd.DataFrame(result)
 
 # print(df.head())
 
 faq_documents = {
-    'llm-zoomcamp': '1m2KexowAXTmexfC5rVTCSnaShvdUQ8Ag2IEiwBDHxN0',
+    'llm-zoomcamp': '1qZjwHkvP0lXHiE4zdbWyUXSVfmVGzougDD6N37bat3E',
 }
 
 
@@ -19,4 +20,8 @@ for course, file_id in faq_documents.items():
     course_documents = read_faq(file_id)
     documents.append({'course': course, 'documents': course_documents})
 
-print(len(documents)) # 1
+# Answer to HW5 q2: 1
+print(len(documents))
+
+
+pprint(documents[0])
